22e7faaf52e0a24c550220deeaab31d994d1a7c7
gahamanyiced <gahamacedr@gmail.com>
2022-03-14 23:27:47 +0200

    complete sponsor and student api

--- controllers/students.js
+++ controllers/students.js
@@ -30,7 +30,7 @@ exports.getStudents = asyncHandler(async(req,res,next)=>{
 // @ Access Private
 exports.addStudent = asyncHandler(async(req,res,next)=>{
   
-    const students = await Course.create(req.body)
+    const students = await Student.create(req.body)
     res.status(200).json({
         success:true,
         data: students
